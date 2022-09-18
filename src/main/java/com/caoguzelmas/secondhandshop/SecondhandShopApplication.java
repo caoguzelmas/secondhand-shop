ec77d59ecb5b2a7eb6b07e0644dfbf04318d06d5
caoguzelmas <caoguzelmas@gmail.com>
2022-09-19 01:07:50 +0300

    Initial commit

---
+++ src/main/java/com/caoguzelmas/secondhandshop/SecondhandShopApplication.java
@@ -0,0 +1,13 @@
+package com.caoguzelmas.secondhandshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondhandShopApplication.class, args);
+	}
+
+}
